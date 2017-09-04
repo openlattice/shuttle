88d635b6df5e5a23e50307b7ca7e3f350e33e1a6
Katherine Bernstein <katherine@openlattice.com>
2017-09-04 14:30:01 -0700

    test

--- src/main/java/com/openlattice/shuttle/Shuttle.java
+++ src/main/java/com/openlattice/shuttle/Shuttle.java
@@ -26,7 +26,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
