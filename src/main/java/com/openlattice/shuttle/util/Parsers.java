e277b6df3367f8d0fde4a05d2acbd54d90d13a39
Kim Engie <kim@openlattice.com>
2018-02-13 10:51:25 -0800

    small change to Parsers, since it was throwing errors if there were empty strings.

--- src/main/java/com/openlattice/shuttle/util/Parsers.java
+++ src/main/java/com/openlattice/shuttle/util/Parsers.java
@@ -1,5 +1,6 @@
 package com.openlattice.shuttle.util;
 
+import org.apache.commons.lang3.StringUtils;
 import org.joda.time.LocalDate;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
@@ -20,7 +21,7 @@ public class Parsers {
 
     public static Integer parseInt( Object obj ) {
         String intStr = getAsString( obj );
-        if ( intStr != null ) {
+        if ( StringUtils.isNotBlank( intStr ) ) {
             try {
                 return Integer.parseInt( intStr );
             } catch ( NumberFormatException e ) {
@@ -56,7 +57,7 @@ public class Parsers {
 
     public static Double parseDouble( Object obj ) {
         String doubleStr = getAsString( obj );
-        if ( doubleStr != null ) {
+        if ( StringUtils.isNotBlank( doubleStr ) ) {
             try {
                 return Double.parseDouble( doubleStr );
             } catch ( NumberFormatException e ) {
