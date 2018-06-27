76926b86286c752ed3a132fa4d9bd25f6fcfc0ea
Joke Durnez <joke.durnez@gmail.com>
2018-06-27 16:41:31 -0700

    add constants, to be moved to API later

--- src/main/java/com/openlattice/shuttle/util/Constants.java
+++ src/main/java/com/openlattice/shuttle/util/Constants.java
@@ -1,4 +1,10 @@
 package com.openlattice.shuttle.util;
 
-public class constants {
-}
+    public final class Constants {
+
+        public static final String TRANSFORMATIONDEFINITION  = "transformationdefinition";
+        public static final String TRANSFORMATION          = "transformation";
+        public static final String PREFIX                  = "prefix";
+        public static final String COLUMN                  = "column";
+
+    }
