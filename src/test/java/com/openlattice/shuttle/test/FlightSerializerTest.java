d773c813cb14c52eaa3b64608dcca34cd6f00d68
Matthew Tamayo-Rios <matthew@openlattice.com>
2018-06-28 23:18:48 -0700

    Fix tests

--- src/test/java/com/openlattice/shuttle/test/FlightSerializerTest.java
+++ src/test/java/com/openlattice/shuttle/test/FlightSerializerTest.java
@@ -19,14 +19,20 @@
 
 package com.openlattice.shuttle.test;
 
+import com.dataloom.mappers.ObjectMappers;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.openlattice.data.serializers.FullQualifiedNameJacksonDeserializer;
 import com.openlattice.data.serializers.FullQualifiedNameJacksonSerializer;
 import com.openlattice.serializer.AbstractJacksonSerializationTest;
 import com.openlattice.shuttle.Flight;
+import java.io.IOException;
+import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class FlightSerializerTest extends AbstractJacksonSerializationTest<Flight> {
-
+    private static ObjectMapper yaml = ObjectMappers.getYamlMapper();
     @Override
     protected Flight getSampleData() {
         return ShuttleTest.getFlight();
@@ -37,9 +43,20 @@ public class FlightSerializerTest extends AbstractJacksonSerializationTest<Fligh
         return Flight.class;
     }
 
+    @Test
+    public void testYaml() throws IOException {
+        Flight expected = getSampleData();
+
+        String yml = yaml.writeValueAsString( expected );
+        Flight actual = yaml.readValue( yml , getClazz());
+        logger.info("Yaml: {}", yml );
+        Assert.assertEquals( expected,actual );
+    }
     @BeforeClass
     public static void registerModules() {
         registerModule( FullQualifiedNameJacksonSerializer::registerWithMapper );
         registerModule( FullQualifiedNameJacksonDeserializer::registerWithMapper );
+        FullQualifiedNameJacksonSerializer.registerWithMapper( yaml );
+        FullQualifiedNameJacksonDeserializer.registerWithMapper( yaml );
     }
 }
