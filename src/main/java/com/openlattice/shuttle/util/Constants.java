7ac1860faefb4a75d62e71c9717d0722a73ec3d8
Joke Durnez <joke.durnez@gmail.com>
2018-07-19 15:35:35 -0700

    add new constants

--- src/main/java/com/openlattice/shuttle/util/Constants.java
+++ src/main/java/com/openlattice/shuttle/util/Constants.java
@@ -12,8 +12,10 @@ public final class Constants {
     public static final String SEP           = "separator";
     public static final String PATTERN       = "pattern";
     public static final String VALUE         = "value";
-    public static final String ELSE          = "else";
+    public static final String ELSE          = "valueelse";
     public static final String LOC           = "location";
     public static final String TEMP          = "temp";
+    public static final String PATTERNS      = "patterns";
+    public static final String INDEX         = "index";
 
 }
