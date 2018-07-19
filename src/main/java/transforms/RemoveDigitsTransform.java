b9eeed61dd2749104f3f736eafe7c4414c8cab60
Joke Durnez <joke.durnez@gmail.com>
2018-07-18 18:11:35 -0700

    remove digits from string transformation

--- src/main/java/transforms/RemoveDigitsTransform.java
+++ src/main/java/transforms/RemoveDigitsTransform.java
@@ -1,4 +1,51 @@
+/*
+ * Copyright (C) 2018. OpenLattice, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * You can contact the owner of the copyright at support@openlattice.com
+ *
+ *
+ */
+
 package transforms;
 
-public class RemoveDigitsTransform {
+import static com.openlattice.shuttle.transformations.Transformation.TRANSFORM;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.openlattice.shuttle.transformations.Transformation;
+import com.openlattice.shuttle.util.Constants;
+
+import java.util.List;
+import java.util.Objects;
+
+@JsonIgnoreProperties(value = {TRANSFORM} )
+public class RemoveDigitsTransform extends Transformation<String>  {
+    private final String tmp;
+
+    @JsonCreator
+    public RemoveDigitsTransform(
+            @JsonProperty( Constants.TEMP ) String tmp
+            ) {
+        this.tmp = tmp;
+    }
+
+    @Override
+    public Object apply( String o ) {
+         return o.replaceAll( "[\\d]+", "" );
+    }
+
 }
