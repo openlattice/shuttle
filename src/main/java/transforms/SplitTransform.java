2f8af0c17ded898ed190b8e73fb373f10a71f03a
Joke Durnez <joke.durnez@gmail.com>
2018-07-19 15:35:59 -0700

    add docs

--- src/main/java/transforms/SplitTransform.java
+++ src/main/java/transforms/SplitTransform.java
@@ -1,4 +1,67 @@
+/*
+ * Copyright (C) 2018. OpenLattice, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * You can contact the owner of the copyright at support@openlattice.com
+ *
+ *
+ */
+
 package transforms;
 
-public class SplitTransform {
+import static com.openlattice.shuttle.transformations.Transformation.TRANSFORM;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.openlattice.shuttle.transformations.Transformation;
+import com.openlattice.shuttle.util.Constants;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@JsonIgnoreProperties(value = {TRANSFORM} )
+
+
+public class SplitTransform extends Transformation<String>  {
+
+    private final String separator;
+    private final int index;
+
+    /**
+     * Represents a transformation to split a string
+     * @param separator: separate by what?
+     * @param index: index in separated list (starts at 0!)
+     */
+    @JsonCreator
+    public SplitTransform(
+            @JsonProperty( Constants.SEP ) String separator ,
+            @JsonProperty (Constants.INDEX) int index
+    ) {
+        this.separator = separator;
+        this.index = index;
+    }
+
+    @Override public Object apply( String o ) {
+        String[] strNames = o.split( separator );
+        if ( strNames.length > index ) {
+            return strNames[ index ].trim();
+        }
+        return "";
+    }
+
 }
