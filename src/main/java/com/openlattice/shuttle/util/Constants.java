4c61889b834b2251c89a2f30dfef2ac8dfef606f
Joke Durnez <joke.durnez@gmail.com>
2018-07-28 12:29:23 -0700

    add transform if true, transform if false + style

--- src/main/java/com/openlattice/shuttle/util/Constants.java
+++ src/main/java/com/openlattice/shuttle/util/Constants.java
@@ -2,21 +2,23 @@ package com.openlattice.shuttle.util;
 
 public final class Constants {
 
-    public static final String COLUMN        = "column";
-    public static final String COLUMNS       = "columns";
-    public static final String HASH_FUNCTION = "hashFunction";
-    public static final String PREFIX        = "prefix";
-    public static final String READER        = "reader";
-    public static final String TRANSFORMS    = "transforms";
-    public static final String TYPE          = "type";
-    public static final String SEP           = "separator";
-    public static final String PATTERN       = "pattern";
-    public static final String VALUE         = "value";
-    public static final String ELSE          = "valueelse";
-    public static final String LOC           = "location";
-    public static final String PATTERNS      = "patterns";
-    public static final String INDEX         = "index";
-    public static final String TARGET        = "target";
-    public static final String GOAL          = "goal";
+    public static final String COLUMN            = "column";
+    public static final String COLUMNS           = "columns";
+    public static final String HASH_FUNCTION     = "hashFunction";
+    public static final String PREFIX            = "prefix";
+    public static final String READER            = "reader";
+    public static final String TRANSFORMS        = "transforms";
+    public static final String TRANSFORMSIFTRUE  = "transformsiftrue";
+    public static final String TRANSFORMSIFFALSE = "transformsiffalse";
+    public static final String TYPE              = "type";
+    public static final String SEP               = "separator";
+    public static final String PATTERN           = "pattern";
+    public static final String VALUE             = "value";
+    public static final String ELSE              = "valueelse";
+    public static final String LOC               = "location";
+    public static final String PATTERNS          = "patterns";
+    public static final String INDEX             = "index";
+    public static final String TARGET            = "target";
+    public static final String GOAL              = "goal";
 
 }
