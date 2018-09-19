2458f70c9c06a9a96035f764a4bb60ba224091df
jokedurnez <joke.durnez@gmail.com>
2018-09-19 16:28:34 -0700

    format

--- src/test/java/com/openlattice/shuttle/transforms/TransformTest.java
+++ src/test/java/com/openlattice/shuttle/transforms/TransformTest.java
@@ -14,27 +14,26 @@ public class TransformTest {
 
     @Test
     public void testPrefixTransform() {
-        Object prefixTest1 = new PrefixTransform( "prefix_" ).apply("name");
+        Object prefixTest1 = new PrefixTransform( "prefix_" ).apply( "name" );
         Assert.assertEquals( "prefix_name", prefixTest1 );
 
-        Object prefixTest2 = new PrefixTransform( "prefix_" ).apply(null);
+        Object prefixTest2 = new PrefixTransform( "prefix_" ).apply( null );
         Assert.assertEquals( null, prefixTest2 );
     }
 
     @Test
     public void testParseIntTransform() {
-        Object parseIntTest1 = new ParseIntTransform(  ).apply("3");
-        Assert.assertEquals( 3 , parseIntTest1 );
+        Object parseIntTest1 = new ParseIntTransform().apply( "3" );
+        Assert.assertEquals( 3, parseIntTest1 );
     }
 
     @Test
     public void testDateTimeTransform() {
-        String[] patterns = {"MM/dd/yyyy HH:mm"};
-        OffsetDateTime expected = OffsetDateTime.of( LocalDateTime.of(2018, 03, 05, 10, 0), ZoneOffset.ofHours( -5 ) );
-        Object dateTimeTest1 = new DateTimeTransform( patterns ).apply("03/05/2018 10:00");
-        Assert.assertEquals( expected , dateTimeTest1 );
+        String[] patterns = { "MM/dd/yyyy HH:mm" };
+        OffsetDateTime expected = OffsetDateTime
+                .of( LocalDateTime.of( 2018, 03, 05, 10, 0 ), ZoneOffset.ofHours( -5 ) );
+        Object dateTimeTest1 = new DateTimeTransform( patterns ).apply( "03/05/2018 10:00" );
+        Assert.assertEquals( expected, dateTimeTest1 );
     }
-
-
 
 }
