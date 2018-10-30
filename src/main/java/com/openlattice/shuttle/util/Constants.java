cfa772dd303222aea4c4ad800c6c53835f43af41
jokedurnez <joke.durnez@gmail.com>
2018-10-29 17:21:56 -0700

    add suffix constant

--- src/main/java/com/openlattice/shuttle/util/Constants.java
+++ src/main/java/com/openlattice/shuttle/util/Constants.java
@@ -6,6 +6,7 @@ public final class Constants {
     public static final String COLUMNS             = "columns";
     public static final String HASH_FUNCTION       = "hashFunction";
     public static final String PREFIX              = "prefix";
+    public static final String SUFFIX              = "suffix";
     public static final String READER              = "reader";
     public static final String TRANSFORMS          = "transforms";
     public static final String TRANSFORMS_IF_TRUE  = "transformsIfTrue";
