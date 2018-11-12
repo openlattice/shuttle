12b2eb51722b2a2d919b7edd152828689e71e57c
jokedurnez <joke.durnez@gmail.com>
2018-11-12 11:02:55 -0800

    case transforms

--- src/main/java/transforms/CaseTransform.java
+++ src/main/java/transforms/CaseTransform.java
@@ -1,17 +1,42 @@
 package transforms;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.openlattice.shuttle.Shuttle;
 import com.openlattice.shuttle.transformations.Transformation;
+import com.openlattice.shuttle.util.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CaseTransform extends Transformation<String> {
 
+    private static final Logger logger = LoggerFactory
+            .getLogger( Shuttle.class );
 
+    private final String type;
+
+    /**
+     * Represents a transformation to add a prefix.
+     *
+     * @param type: how to case: name, lower, upper
+     */
     @JsonCreator
-    public CaseTransform() {}
+    public CaseTransform(@JsonProperty( Constants.TYPE) String type) {
+        this.type = type;
+    }
 
     @Override
     public Object apply( String o ) {
-        return o.substring(0, 1).toUpperCase() + o.substring(1).toLowerCase();
+        if (this.type == "name") {
+            return o.substring(0, 1).toUpperCase() + o.substring(1).toLowerCase();
+        }  else if (this.type == "lower") {
+            return o.toLowerCase() ;
+        } else if (this.type == "upper") {
+            return o.toUpperCase();
+        } else {
+            logger.error("Unknown type of caseing: "+type);
+        }
+        return null;
     }
 
  }
