49a91dbbfc0e686b129f891f11a0d72fc277c6fb
Matthew Tamayo-Rios <matthew@openlattice.com>
2019-01-18 16:29:34 -0800

    Log progress in result set.

--- src/main/java/com/openlattice/shuttle/payload/JdbcPayload.java
+++ src/main/java/com/openlattice/shuttle/payload/JdbcPayload.java
@@ -3,6 +3,7 @@ package com.openlattice.shuttle.payload;
 import com.dataloom.streams.StreamUtil;
 import com.google.common.util.concurrent.RateLimiter;
 import com.zaxxer.hikari.HikariDataSource;
+import java.util.concurrent.atomic.AtomicLong;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,19 +18,24 @@ import java.util.stream.Stream;
  * @author Matthew Tamayo-Rios &lt;matthew@openlattice.com&gt;
  */
 public class JdbcPayload implements Payload {
-    private static final Logger           logger = LoggerFactory.getLogger( JdbcPayload.class );
-    private final        HikariDataSource hds;
-    private final        String           sql;
-    private final        RateLimiter      rateLimiter;
+    private static final Logger logger                     = LoggerFactory.getLogger( JdbcPayload.class );
+    private static final int    DEFAULT_FETCH_SIZE         = 50000;
+    private static final double DEFAULT_PERMITS_PER_SECOND = 10000.0;
+
+    private final HikariDataSource hds;
+    private final String           sql;
+    private final RateLimiter      rateLimiter;
+    private final int              fetchSize;
 
     public JdbcPayload( HikariDataSource hds, String sql ) {
-        this( hds, sql, 10000 );
+        this( hds, sql, DEFAULT_PERMITS_PER_SECOND, DEFAULT_FETCH_SIZE );
     }
 
-    public JdbcPayload( HikariDataSource hds, String sql, double permitsPerSecond ) {
+    public JdbcPayload( HikariDataSource hds, String sql, double permitsPerSecond, int fetchSize ) {
         this.rateLimiter = RateLimiter.create( permitsPerSecond );
         this.hds = hds;
         this.sql = sql;
+        this.fetchSize = fetchSize;
     }
 
     @Override public Stream<Map<String, Object>> getPayload() {
@@ -38,7 +44,7 @@ public class JdbcPayload implements Payload {
             Statement statement = conn.createStatement();
             statement.setFetchSize( 50000 );
             final ResultSet rs = statement.executeQuery( sql );
-            return StreamUtil.stream( () -> new ResultSetStringIterator( conn, statement, rs, rateLimiter ) );
+            return StreamUtil.stream( () -> new ResultSetStringIterator( conn, statement, rs, rateLimiter, sql ) );
         } catch ( SQLException e ) {
             logger.info( "Unable to get payload.", e );
         }
@@ -46,23 +52,29 @@ public class JdbcPayload implements Payload {
     }
 
     static final class ResultSetStringIterator implements Iterator<Map<String, Object>> {
+        private static long READ_COUNT_THRESHOLD = 100000L;
+
         private final Connection    connection;
         private final Statement     stmt;
         private final ResultSet     rs;
         private final List<String>  columns;
         private final int           columnCount;
         private final RateLimiter   rateLimiter;
-        private       AtomicBoolean hasNext = new AtomicBoolean( false );
+        private final String        sql;
+        private final AtomicLong    readCount = new AtomicLong( 0 );
+        private       AtomicBoolean hasNext   = new AtomicBoolean( false );
 
         public ResultSetStringIterator(
                 Connection connection,
                 Statement stmt,
                 ResultSet rs,
-                RateLimiter rateLimiter ) {
+                RateLimiter rateLimiter,
+                String sql ) {
             this.connection = connection;
             this.stmt = stmt;
             this.rs = rs;
             this.rateLimiter = rateLimiter;
+            this.sql = sql;
             ResultSetMetaData rsm = null;
             try {
                 rsm = rs.getMetaData();
@@ -95,6 +107,10 @@ public class JdbcPayload implements Payload {
             } catch ( SQLException e ) {
                 logger.error( "Unable to advance to next item.", e );
             }
+            final var currentReadCount = readCount.incrementAndGet();
+            if ( ( currentReadCount % READ_COUNT_THRESHOLD ) == 0 ) {
+                logger.info( "{} rows have been read for the query {}", currentReadCount, sql );
+            }
             return data;
         }
 
@@ -103,7 +119,7 @@ public class JdbcPayload implements Payload {
                 Object val = "";
                 try {
                     Object obj = rs.getObject( col );
-                    if (obj instanceof byte[]){
+                    if ( obj instanceof byte[] ) {
                         val = obj;
                     } else if ( obj != null ) { val = obj.toString(); }
                 } catch ( SQLException e ) {
