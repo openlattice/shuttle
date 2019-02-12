7ac32694e6a89cd61a0ee8445ef398345b82f3a4
Matthew Tamayo-Rios <matthew@openlattice.com>
2019-02-11 20:20:45 -0800

    Retry aws uploads

--- src/main/kotlin/com/openlattice/data/integration/destinations/S3Destination.kt
+++ src/main/kotlin/com/openlattice/data/integration/destinations/S3Destination.kt
@@ -26,8 +26,16 @@ import com.openlattice.data.integration.*
 import com.openlattice.data.integration.Entity
 import com.openlattice.data.util.PostgresDataHasher
 import org.apache.olingo.commons.api.edm.EdmPrimitiveTypeKind
+import org.slf4j.LoggerFactory
+import java.lang.ClassCastException
+import java.lang.Exception
 import java.util.*
+import java.util.concurrent.locks.ReentrantLock
 import java.util.function.Supplier
+import java.util.stream.Collectors
+import kotlin.streams.asSequence
+
+private val logger = LoggerFactory.getLogger(S3Destination::class.java)
 
 /**
  *
@@ -41,8 +49,9 @@ class S3Destination(
     override fun integrateEntities(
             data: Set<Entity>, entityKeyIds: Map<EntityKey, UUID>, updateTypes: Map<UUID, UpdateType>
     ): Long {
-        val (s3entities, values) = data.flatMap { entity ->
-            val entityKeyId = entityKeyIds[entity.key]!!
+//        val (s3entities, values)
+               val s3entities = data.flatMap { entity ->
+            val entityKeyId = entityKeyIds.getValue(entity.key)
             entity.details.entries.flatMap { (propertyTypeId, properties) ->
                 properties.map {
                     S3EntityData(
@@ -53,15 +62,39 @@ class S3Destination(
                     ) to it
                 }
             }
-        }.unzip()
+        }
 
-        dataIntegrationApi
-                .generatePresignedUrls(s3entities)
-                .zip(values)
-                .parallelStream()
-                .forEach { s3Api.writeToS3(it.first, it.second as ByteArray) }
+        var s3eds = s3entities
 
-        return values.size.toLong()
+        while( s3eds.isNotEmpty() ) {
+            val (s3entities, values) = s3eds.unzip()
+            val presignedUrls = dataIntegrationApi.generatePresignedUrls(s3entities)
+
+            s3eds = s3entities
+                    .mapIndexed { index, s3EntityData ->
+                        Triple(s3EntityData, presignedUrls[index], values[index])
+                    }
+                    .parallelStream()
+                    .filter { (s3ed, url, bytes) ->
+                        try {
+                            s3Api.writeToS3(url, bytes as ByteArray)
+                            false
+                        } catch (ex: Exception) {
+                            if (ex is ClassCastException) {
+                                logger.error("Expected byte array, but found wrong data type for upload.", ex)
+                                kotlin.system.exitProcess(2)
+                            } else {
+                                logger.error("Encountered an issue when uploading data. Retrying...", ex)
+                                true
+                            }
+                        }
+                    }
+                    .map {(s3ed, _, bytes ) ->
+                        s3ed to bytes
+                    }.collect(Collectors.toList())
+        }
+
+        return s3entities.size.toLong()
     }
 
     override fun integrateAssociations(
