5cc289d304064df2144a79c1d2900fffd5ba8ece
Drew <drew@openlattice.com>
2019-04-30 14:09:22 -0400

    no more woodstox

--- src/main/kotlin/com/openlattice/shuttle/payload/XmlPayload.kt
+++ src/main/kotlin/com/openlattice/shuttle/payload/XmlPayload.kt
@@ -1,9 +1,6 @@
 package com.openlattice.shuttle.payload
 
-import com.ctc.wstx.stax.WstxInputFactory
-import com.ctc.wstx.stax.WstxOutputFactory
 import com.fasterxml.jackson.core.type.TypeReference
-import com.fasterxml.jackson.dataformat.xml.XmlFactory
 import com.fasterxml.jackson.dataformat.xml.XmlMapper
 import org.slf4j.LoggerFactory
 import java.io.File
@@ -15,15 +12,7 @@ data class XmlPayload( val directoryPath: String ) : Payload {
 
     private val logger = LoggerFactory.getLogger(XmlPayload::class.java)
 
-    private val xof = WstxOutputFactory()
-    private val xif = WstxInputFactory()
-    init {
-        xof.configureForSpeed()
-        xif.configureForSpeed()
-    }
-    private val xf = XmlFactory(xif, xof );
-
-    private val mapper = XmlMapper( xf )
+    private val mapper = XmlMapper()
 
     override fun getPayload(): Stream<MutableMap<String, Any>> {
         if (!File( directoryPath ).isDirectory){
