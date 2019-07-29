6a960dfdfe8056ed3de66c5a5367048ec6965bb2
jokedurnez <joke.durnez@gmail.com>
2019-07-28 21:48:44 -0700

    nullfix

--- src/main/java/transforms/NestedTransformsTransform.java
+++ src/main/java/transforms/NestedTransformsTransform.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.openlattice.shuttle.transformations.TransformValueMapper;
 import com.openlattice.shuttle.transformations.Transformation;
 import com.openlattice.shuttle.util.Constants;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 import java.util.Map;
@@ -28,7 +29,13 @@ public class NestedTransformsTransform extends Transformation<Map<String, Object
     @Override
     public Object apply( Map<String, Object> row ) {
 
-        String outstring = this.transformValueMapper.apply( row ).toString();
+        Object outobj = this.transformValueMapper.apply( row );
+
+        if ( outobj == null ) {
+            return null;
+        }
+
+        String outstring = outobj.toString();
 
         return ( outstring );
     }
