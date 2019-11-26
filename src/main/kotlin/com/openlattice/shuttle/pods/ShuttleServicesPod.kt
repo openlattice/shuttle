78e628a19f0861fd7fdf9ea049d95fa005ed8af0
Piper Keyes <piper@openlattice.com>
2019-11-25 17:45:19 -0800

    first bean

--- src/main/kotlin/com/openlattice/shuttle/pods/ShuttleServicesPod.kt
+++ src/main/kotlin/com/openlattice/shuttle/pods/ShuttleServicesPod.kt
@@ -1,11 +1,25 @@
 package com.openlattice.shuttle.pods
 
+import com.openlattice.jdbc.JdbcPod
+import com.openlattice.shuttle.RecurringIntegrationService
+import com.zaxxer.hikari.HikariDataSource
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
+import javax.inject.Inject
 
 /**
  *
  * @author Matthew Tamayo-Rios &lt;matthew@openlattice.com&gt;
  */
 @Configuration
+@Import( value = [JdbcPod::class] )
 class ShuttleServicesPod {
+
+    @Inject
+    private lateinit var hds: HikariDataSource
+
+    @Bean
+    fun recurringIntegrationService() = RecurringIntegrationService(hds)
+
 }
