9f8d3a786e26dd7b570f622a3ed1c8703c3d9e39
Piper Keyes <piper@openlattice.com>
2019-11-26 16:39:23 -0800

    ensure admin

--- src/main/kotlin/com/openlattice/shuttle/controllers/ShuttleController.kt
+++ src/main/kotlin/com/openlattice/shuttle/controllers/ShuttleController.kt
@@ -1,27 +1,36 @@
 package com.openlattice.shuttle.controllers
 
 import com.codahale.metrics.annotation.Timed
+import com.openlattice.authorization.AuthorizationManager
+import com.openlattice.authorization.AuthorizingComponent
 import com.openlattice.shuttle.*
-import com.openlattice.shuttle.api.CONTROLLER
-import com.openlattice.shuttle.api.FLIGHT
-import com.openlattice.shuttle.api.ShuttleApi
+import com.openlattice.shuttle.api.*
 import org.springframework.web.bind.annotation.*
+import java.util.*
 import javax.inject.Inject
 
 @RestController
 @RequestMapping(CONTROLLER)
-class ShuttleController : ShuttleApi {
+class ShuttleController : ShuttleApi, AuthorizingComponent {
 
     @Inject
     private lateinit var recurringIntegrationService: RecurringIntegrationService
 
+    @Inject
+    private lateinit var authorizationManager: AuthorizationManager
+
     @Timed
-    @PatchMapping(path = [FLIGHT])
+    @PatchMapping(path = [EKID_PATH])
     override fun startIntegration(
-            @PathVariable(FLIGHT) flightName: String,
+            @PathVariable(EKID) entityKeyId: UUID,
             @RequestBody lastRow: String
     ) {
-        recurringIntegrationService.loadCargo(flightName, lastRow)
+        ensureAdminAccess()
+        recurringIntegrationService.loadCargo(entityKeyId, lastRow)
+    }
+
+    override fun getAuthorizationManager(): AuthorizationManager {
+        return authorizationManager
     }
 
 }
