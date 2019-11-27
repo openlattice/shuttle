96f1e7e8e2e2b03e5d60a8fa06c3524710fcc643
Piper Keyes <piper@openlattice.com>
2019-11-26 16:38:55 -0800

    use entity key id instead of flight name

--- src/main/kotlin/com/openlattice/shuttle/api/ShuttleApi.kt
+++ src/main/kotlin/com/openlattice/shuttle/api/ShuttleApi.kt
@@ -3,25 +3,26 @@ package com.openlattice.shuttle.api
 import retrofit2.http.Body
 import retrofit2.http.PATCH
 import retrofit2.http.Path
+import java.util.*
 
 
 const val SERVICE = "/shuttle"
 const val CONTROLLER = "/integration"
 const val BASE = SERVICE + CONTROLLER
 
-const val FLIGHT = "flight"
-const val FLIGHT_PATH = "{$FLIGHT}"
+const val EKID = "entityKeyId"
+const val EKID_PATH = "{$EKID}"
 
 interface ShuttleApi {
 
     /**
      * Starts an integration on Shuttle Server for a given flight
-     * @param flightName the name of the flight to be run
+     * @param entityKeyId the entity key id of the flight to be run (within the flights entity set)
      * @param lastRow the last row that has already been integrated
      */
-    @PATCH(BASE + FLIGHT_PATH)
+    @PATCH(BASE + EKID_PATH)
     fun startIntegration(
-            @Path(FLIGHT) flightName: String,
+            @Path(EKID) entityKeyId: UUID,
             @Body lastRow: String //not accurate, will need something better once we know what we need
     )
 
