93a77d3901498511176c6b47c87fac69ca97e354
Piper Keyes <piper@openlattice.com>
2019-12-10 17:24:29 -0800

    enable authorization checks

--- src/main/kotlin/com/openlattice/shuttle/pods/ShuttleServicesPod.kt
+++ src/main/kotlin/com/openlattice/shuttle/pods/ShuttleServicesPod.kt
@@ -1,18 +1,18 @@
 package com.openlattice.shuttle.pods
 
 import com.dataloom.mappers.ObjectMappers
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.common.eventbus.EventBus
 import com.hazelcast.core.HazelcastInstance
-import com.openlattice.authorization.AuthorizationManager
-import com.openlattice.authorization.AuthorizationQueryService
-import com.openlattice.authorization.HazelcastAuthorizationService
+import com.openlattice.authorization.*
+import com.openlattice.organizations.roles.HazelcastPrincipalService
+import com.openlattice.organizations.roles.SecurePrincipalsManager
 import com.openlattice.shuttle.MissionParameters
 import com.openlattice.shuttle.RecurringIntegrationService
 import com.openlattice.shuttle.mapstore.IntegrationsMapstore
 import com.zaxxer.hikari.HikariDataSource
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import javax.annotation.PostConstruct
 import javax.inject.Inject
 
 /**
@@ -48,5 +48,20 @@ class ShuttleServicesPod {
 
     @Bean
     fun integrationsMapstore() = IntegrationsMapstore(hds)
+
+    @Bean
+    fun aclKeyReservationService() = HazelcastAclKeyReservationService(hazelcastInstance)
+
+    @Bean
+    fun principalService(): SecurePrincipalsManager = HazelcastPrincipalService(
+            hazelcastInstance,
+            aclKeyReservationService(),
+            authorizationManager(),
+            eventBus)
+
+    @PostConstruct
+    internal fun initPrincipals() {
+        Principals.init(principalService())
+    }
 
 }
