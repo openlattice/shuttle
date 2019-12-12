4fc47ffddf9b08add06c05a0783d83dbf0d551c3
Piper Keyes <piper@openlattice.com>
2019-12-12 11:24:05 -0800

    actually run shuttle

--- src/main/kotlin/com/openlattice/shuttle/RecurringIntegrationService.kt
+++ src/main/kotlin/com/openlattice/shuttle/RecurringIntegrationService.kt
@@ -1,8 +1,23 @@
 package com.openlattice.shuttle
 
+import com.dataloom.mappers.ObjectMappers
 import com.google.common.base.Preconditions.checkState
 import com.hazelcast.core.HazelcastInstance
+import com.openlattice.client.ApiClient
+import com.openlattice.data.DataIntegrationApi
+import com.openlattice.data.S3Api
+import com.openlattice.data.integration.IntegrationDestination
+import com.openlattice.data.integration.StorageDestination
+import com.openlattice.data.integration.destinations.PostgresDestination
+import com.openlattice.data.integration.destinations.PostgresS3Destination
+import com.openlattice.edm.EntitySet
+import com.openlattice.edm.type.EntityType
+import com.openlattice.edm.type.PropertyType
 import com.openlattice.hazelcast.HazelcastMap
+import com.openlattice.retrofit.RhizomeByteConverterFactory
+import com.openlattice.retrofit.RhizomeCallAdapterFactory
+import com.openlattice.retrofit.RhizomeJacksonConverterFactory
+import com.openlattice.rhizome.proxy.RetrofitBuilders
 import com.openlattice.shuttle.control.Integration
 import com.openlattice.shuttle.hazelcast.processors.ReloadFlightEntryProcessor
 import com.openlattice.shuttle.payload.JdbcPayload
@@ -10,6 +25,7 @@ import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import retrofit2.Retrofit
 import java.lang.Exception
 import java.util.*
 
@@ -25,67 +41,96 @@ class RecurringIntegrationService(
 ) {
 
     private val integrations = hazelcastInstance.getMap<String, Integration>(HazelcastMap.INTEGRATIONS.name)
+    private val entitySets = hazelcastInstance.getMap<UUID, EntitySet>(HazelcastMap.ENTITY_SETS.name)
+    private val entityTypes = hazelcastInstance.getMap<UUID, EntityType>(HazelcastMap.ENTITY_TYPES.name)
+    private val propertyTypes = hazelcastInstance.getMap<UUID, PropertyType>(HazelcastMap.PROPERTY_TYPES.name)
+    private val config = missionParameters.postgres.config
 
     fun loadCargo(integrationName: String) {
         checkState(integrations.containsKey(integrationName), "Integration with name $integrationName does not exist")
         val integration = integrations.getValue(integrationName)
-        val dataSource = getDataSource(integration.source)
-        val payload = JdbcPayload(readRateLimit.toDouble(), dataSource, integration.sql, fetchSize, readRateLimit != 0)
+
+        //THIS IS WRONG. Need to somehow get a token or username/password
+        //val authToken = MissionControl.getIdToken(config.getProperty("username"), config.getProperty("password"))
+        val apiClient = ApiClient(integration.environment) { "i'matoken" }
+        val dataIntegrationApi = apiClient.dataIntegrationApi
+
+        val srcDataSource = getSrcDataSource(integration.source)
+        val payload = JdbcPayload(readRateLimit.toDouble(), srcDataSource, integration.sql, fetchSize, readRateLimit != 0)
         val flightPlan = mapOf(integration.flight!! to payload)
+        val destinationsMap = generateDestinationsMap(integration, missionParameters, dataIntegrationApi)
 
         try {
-            val config = missionParameters.postgres.config
-            val missionControl = MissionControl(
+            val shuttle = Shuttle(
                     integration.environment,
-                    config.getProperty("username"),
-                    config.getProperty("password"),
-                    integration.s3bucket,
-                    missionParameters)
-            logger.info("Preparing flight plan.")
-            val shuttle = missionControl.prepare(flightPlan,
-                    false,
+                    flightPlan,
+                    entitySets.values.associateBy { it.name },
+                    entityTypes.values.associateBy { it.id },
+                    propertyTypes.values.associateBy { it.type },
+                    destinationsMap,
+                    dataIntegrationApi,
                     integration.sourcePrimaryKeyColumns,
-                    integration.contacts)
-            logger.info("Pre-flight check list complete.")
+                    missionParameters,
+                    StorageDestination.S3
+            )
             shuttle.launch(uploadBatchSize)
-            MissionControl.succeed()
+            logger.info("YOU DID IT :D")
         } catch (ex: Exception) {
-            MissionControl.fail(1, integration.flight!!, ex)
+            logger.info("You did not do it :'(, failed with exception $ex")
         }
     }
 
     fun createIntegrationDefinition(integrationName: String, integration: Integration) {
-        checkState( !integrations.containsKey(integrationName), "An integration with name $integrationName already exists." )
+        checkState(!integrations.containsKey(integrationName), "An integration with name $integrationName already exists.")
         integrations[integrationName] = integration
     }
 
-    fun readIntegrationDefinition(integrationName: String) : Integration {
-        checkState( integrations.containsKey(integrationName), "Integration with name $integrationName does not exist.")
+    fun readIntegrationDefinition(integrationName: String): Integration {
+        checkState(integrations.containsKey(integrationName), "Integration with name $integrationName does not exist.")
         return integrations.getValue(integrationName)
     }
 
     fun updateIntegrationDefinition(integrationName: String, integration: Integration) {
-        checkState( integrations.containsKey(integrationName), "Integration with name $integrationName does not exist." )
+        checkState(integrations.containsKey(integrationName), "Integration with name $integrationName does not exist.")
         integrations[integrationName] = integration
     }
 
-//    fun updateFlightWithinIntegrationDefinition(integrationName: String, pathToFlight: String) {
-//        checkState( integrations.containsKey(integrationName), "Integration with name $integrationName does not exist." )
-//        //will need to do some fancy indexing on the flight within the integration json
-//    }
-
     fun updateFlightWithinIntegrationDefinition(integrationName: String) {
-        checkState( integrations.containsKey(integrationName), "Integration with name $integrationName does not exist." )
+        checkState(integrations.containsKey(integrationName), "Integration with name $integrationName does not exist.")
         integrations.executeOnKey(integrationName, ReloadFlightEntryProcessor())
     }
 
     fun deleteIntegrationDefinition(integrationName: String) {
-        checkState( integrations.containsKey(integrationName), "Integration with name $integrationName does not exist." )
+        checkState(integrations.containsKey(integrationName), "Integration with name $integrationName does not exist.")
         integrations[integrationName] = null
     }
 
-    private fun getDataSource(properties: Properties): HikariDataSource {
-        return HikariDataSource(HikariConfig(properties))
+    private fun getSrcDataSource(source: Properties): HikariDataSource {
+        return HikariDataSource(HikariConfig(source))
+    }
+
+    private fun generateDestinationsMap(integration: Integration, missionParameters: MissionParameters, dataIntegrationApi: DataIntegrationApi): Map<StorageDestination, IntegrationDestination> {
+        val s3BucketUrl = integration.s3bucket
+
+        val pgDestination = PostgresDestination(
+                entitySets.mapKeys { it.value.id },
+                entityTypes,
+                propertyTypes.mapKeys { it.value.id },
+                HikariDataSource(HikariConfig(missionParameters.postgres.config))
+        )
+
+        if (s3BucketUrl.isNotBlank()) {
+            val s3Api = Retrofit.Builder()
+                    .baseUrl(s3BucketUrl)
+                    .addConverterFactory(RhizomeByteConverterFactory())
+                    .addConverterFactory(RhizomeJacksonConverterFactory(ObjectMappers.getJsonMapper()))
+                    .addCallAdapterFactory(RhizomeCallAdapterFactory())
+                    .client(RetrofitBuilders.okHttpClient().build())
+                    .build().create(S3Api::class.java)
+            val s3Destination = PostgresS3Destination(pgDestination, s3Api, dataIntegrationApi)
+            return mapOf(StorageDestination.POSTGRES to pgDestination, StorageDestination.S3 to s3Destination)
+        }
+        return mapOf(StorageDestination.POSTGRES to pgDestination)
     }
 
 }
