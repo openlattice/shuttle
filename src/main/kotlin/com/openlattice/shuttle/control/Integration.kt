622a682332bc5992fde44466ee4963bc066e91bd
Piper Keyes <piper@openlattice.com>
2019-12-13 17:08:21 -0800

    change vals to vars and read flight yaml from url

--- src/main/kotlin/com/openlattice/shuttle/control/Integration.kt
+++ src/main/kotlin/com/openlattice/shuttle/control/Integration.kt
@@ -9,6 +9,8 @@ import com.openlattice.data.integration.StorageDestination
 import com.openlattice.mapstores.TestDataFactory
 import com.openlattice.shuttle.Flight
 import java.io.File
+import java.net.URI
+import java.net.URL
 import java.util.*
 
 /**
@@ -23,6 +25,9 @@ import java.util.*
  * @param environment the retrofit environment (e.g. production, local)
  * @param defaultStorage ?????
  * @param s3bucket the url of the s3bucket to be used
+ * @param flightFilePath the path to the flight yaml. If you would like the flight to
+ * be reloadable from this path, please store flight yaml on github and provide the path
+ * to it (i.e. https://raw.githubusercontent.com/pathToFlight.yaml)
  * @param flight Flight object
  * @param contacts the set of email addresses of those responsible for the integration
  * @param recurring boolean denoting if the integration is recurring
@@ -30,23 +35,25 @@ import java.util.*
  * @param period ??? how often it gets run?
  */
 data class Integration(
-        @JsonProperty(SerializationConstants.SQL) val sql: String,
-        @JsonProperty(SerializationConstants.SRC) val source: Properties,
-        @JsonProperty(SerializationConstants.SRC_PKEY_COLUMNS) val sourcePrimaryKeyColumns: List<String> = listOf(),
-        @JsonProperty(SerializationConstants.ENVIRONMENT) val environment: RetrofitFactory.Environment,
-        @JsonProperty(SerializationConstants.DEFAULT_STORAGE) val defaultStorage: StorageDestination,
-        @JsonProperty(SerializationConstants.S3_BUCKET) val s3bucket: String,
-        @JsonProperty(SerializationConstants.PATH) val flightFilePath: String?,
+        @JsonProperty(SerializationConstants.SQL) var sql: String,
+        @JsonProperty(SerializationConstants.SRC) var source: Properties,
+        @JsonProperty(SerializationConstants.SRC_PKEY_COLUMNS) var sourcePrimaryKeyColumns: List<String> = listOf(),
+        @JsonProperty(SerializationConstants.ENVIRONMENT) var environment: RetrofitFactory.Environment,
+        @JsonProperty(SerializationConstants.DEFAULT_STORAGE) var defaultStorage: StorageDestination,
+        @JsonProperty(SerializationConstants.S3_BUCKET) var s3bucket: String,
+        @JsonProperty(SerializationConstants.PATH) var flightFilePath: String?,
         @JsonProperty(SerializationConstants.FLIGHT) var flight: Flight?,
-        @JsonProperty(SerializationConstants.CONTACTS) val contacts: Set<String>,
-        @JsonProperty(SerializationConstants.RECURRING) val recurring: Boolean,
-        @JsonProperty(SerializationConstants.START) val start: Long,
-        @JsonProperty(SerializationConstants.PERIOD) val period: Long
+        @JsonProperty(SerializationConstants.CONTACTS) var contacts: Set<String>,
+        @JsonProperty(SerializationConstants.RECURRING) var recurring: Boolean,
+        @JsonProperty(SerializationConstants.START) var start: Long,
+        @JsonProperty(SerializationConstants.PERIOD) var period: Long
 ) {
     init {
         if (flightFilePath == null) check(flight != null) {"Either flight or flightFilePath must not be null"}
         if (flight == null) check(flightFilePath != null) {"Either flight or flightFilePath must not be null"}
-        if (flightFilePath != null && flight == null) this.flight = ObjectMappers.getYamlMapper().readValue(File(flightFilePath), Flight::class.java)
+        if (flightFilePath != null && flight == null) {
+            this.flight = ObjectMappers.getYamlMapper().readValue(URL(flightFilePath!!), Flight::class.java)
+        }
     }
 
     companion object {
