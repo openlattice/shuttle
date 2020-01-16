be4d1fb2ba03ec3b00a31111c66f32fd5244a779
Piper Keyes <piper@openlattice.com>
2020-01-16 10:40:44 -0800

    properties continue to be taken down

--- src/main/kotlin/com/openlattice/shuttle/control/FlightPlanParametersUpdate.kt
+++ src/main/kotlin/com/openlattice/shuttle/control/FlightPlanParametersUpdate.kt
@@ -6,7 +6,7 @@ import java.util.*
 
 data class FlightPlanParametersUpdate(
         @JsonProperty(SerializationConstants.SQL) var sql: Optional<String>,
-        @JsonProperty(SerializationConstants.SRC) var source: Optional<Properties>,
+        @JsonProperty(SerializationConstants.SRC) var source: Optional<Map<String, String>>,
         @JsonProperty(SerializationConstants.SRC_PKEY_COLUMNS) var sourcePrimaryKeyColumns: Optional<List<String>>,
         @JsonProperty(SerializationConstants.PATH) var flightFilePath: Optional<String>
 )
