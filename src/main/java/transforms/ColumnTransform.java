dd271dc8aaa0060c1e67f9fab6e406effe508314
Piper Keyes <piper@openlattice.com>
2020-02-12 18:05:37 -0800

    bring it back

---
+++ src/main/java/transforms/ColumnTransform.java
@@ -0,0 +1,21 @@
+package transforms;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.openlattice.shuttle.transformations.Transformation;
+import com.openlattice.shuttle.util.Constants;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class ColumnTransform extends Transformation<Map<String, String>> {
+    /**
+     * Represents a transformation to select a column in the original data (i.e. no transform)
+     *
+     * @param column: column name to collect
+     */
+    @JsonCreator
+    public ColumnTransform( @JsonProperty( Constants.COLUMN ) String column ) {
+        super( Optional.of(column));
+    }
+}
